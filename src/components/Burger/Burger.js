23594a9b4ed11a257efc644c98bd64c12dc3fa55
Pravin Xeona <pravin.xeona21@gmail.com>
2021-03-26 13:08:31 +1100

    Implemented dynamic addition of ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,15 @@ import styles from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredients/BurgerIngredients";
 
 const burger = (props) => {
+  const ingredientItems = Object.keys(props.ingredients).map((ingKey) =>
+    [...Array(props.ingredients[ingKey])].map((_, k) => (
+      <BurgerIngredient key={ingKey + k} type={ingKey} />
+    ))
+  );
   return (
     <div className={styles.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="bacon" />
+      {ingredientItems}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,19 @@ import Aux from "../../hoc/Auxiliary";
 import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      meat: 1,
+      cheese: 2,
+      salad: 1,
+      bacon: 2,
+    },
+  };
+
   render() {
     return (
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Builder controls</div>
       </Aux>
     );
