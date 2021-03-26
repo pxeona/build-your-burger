fb505d648429fe7b516fa6760c77e8ed137694c2
Pravin Xeona <pravin.xeona21@gmail.com>
2021-03-26 12:31:40 +1100

    Added the burger component to the burger builder page

--- src/components/Burger/BurgerIngredients/BurgerIngredients.js
+++ src/components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -9,6 +9,7 @@ class BurgerIngredient extends Component {
     switch (this.props.type) {
       case "bread-bottom":
         ingredient = <div className={styles.BreadBottom}></div>;
+        break;
       case "bread-top":
         ingredient = (
           <div className={styles.BreadTop}>
@@ -16,25 +17,28 @@ class BurgerIngredient extends Component {
             <div className={styles.Seeds2}></div>
           </div>
         );
+        break;
       case "meat":
         ingredient = <div className={styles.Meat}></div>;
+        break;
       case "cheese":
         ingredient = <div className={styles.Cheese}></div>;
+        break;
       case "salad":
         ingredient = <div className={styles.Salad}></div>;
+        break;
       case "bacon":
         ingredient = <div className={styles.Bacon}></div>;
+        break;
       default:
         ingredient = null;
-
     }
     return ingredient;
-
   }
 }
 
-BurgerIngredient.proptypes = {
-    type: PropTypes.string.isRequired;
+BurgerIngredient.propTypes = {
+  type: PropTypes.string.isRequired,
 };
 
 export default BurgerIngredient;
