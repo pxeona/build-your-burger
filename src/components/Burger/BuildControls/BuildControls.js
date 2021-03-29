728c37175c46240137f5f8f88940dddc9c38c973
Pravin Xeona <pravin.xeona21@gmail.com>
2021-03-29 14:49:14 +1100

    Added a panel to add/remove ingredients

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,9 +1,23 @@
 import React from "react";
 
 import styles from "./BuildControls.module.css";
+import BuildControl from "./BuildControl/BuildControl";
+
+const controls = [
+  { label: "Salad", type: "salad" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Meat", type: "meat" },
+];
 
 const buildControls = (props) => {
-  return null;
+  return (
+    <div className={styles.BuildControls}>
+      {controls.map((control) => (
+        <BuildControl key={control.label} label={control.label} />
+      ))}
+    </div>
+  );
 };
 
 export default buildControls;
