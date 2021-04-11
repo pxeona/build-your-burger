f60190a1453e7f76abb1d023b4973f76cc0d5a6a
Pravin Xeona <pravin.xeona21@gmail.com>
2021-04-11 21:50:14 +1000

    Added a responsive side drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,10 +3,12 @@ import React from "react";
 import Aux from "../../hoc/Auxiliary";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import styles from "./Layout.module.css";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const layout = (props) => (
   <Aux>
     <Toolbar />
+    <SideDrawer />
     <main className={styles.Content}>{props.children}</main>
   </Aux>
 );
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -7,8 +7,10 @@ import NavigationItems from "../NavigationItems/NavigationItems";
 const toolbar = () => (
   <header className={styles.Toolbar}>
     <div>MENU</div>
-    <Logo />
-    <nav>
+    <div className={styles.Logo}>
+      <Logo />
+    </div>
+    <nav className={styles.mobileOnly}>
       <NavigationItems />
     </nav>
   </header>
